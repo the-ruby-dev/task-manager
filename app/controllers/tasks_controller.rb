f0c77dd81022a2dabf0e1350b0fbf8370447c8a9
Davy Malone <85890680+davymalone@users.noreply.github.com>
2024-01-13 23:15:08 +0000

    Styling for create and edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -65,6 +65,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.fetch(:task, {})
+      params.require(:task).permit(:title, :description)
     end
 end
